2c94fa515ff035ce587e5c842ab1e9e24cfe0511
soph8111 <soph8111@stud.kea.dk>
2020-12-14 20:23:09 +0100

    opdatering på index

---
+++ javascript/index.js
@@ -0,0 +1,64 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+
+    // Gem pop op
+    document.querySelector("#popop").classList.add("hide");
+
+    // Vis pop op når man trykker på pil
+    document.querySelector("#pil").addEventListener("click", visPopop);
+
+    // Vis pop op efter 15 sekunder (hvis nu brugeren ikke bruger "pil"-knappen)
+    setTimeout(visPopop, 30000);
+}
+
+function visPopop() {
+    console.log("visPopop");
+
+    // Ryd op
+    document.querySelector("#pil").removeEventListener("click", visPopop);
+
+    // Vis pop op
+    document.querySelector("#popop").classList.remove("hide");
+
+    // Gør det muligt at lukke pop op
+    document.querySelector("#luk").addEventListener("click", lukPopop);
+
+}
+
+function lukPopop() {
+    console.log("lukPopop");
+
+    // Ryd op:
+    document.querySelector("#luk").removeEventListener("click", lukPopop);
+
+    // Skjul pop op
+    document.querySelector("#popop").classList.add("hide");
+}
+
+// Kodning til slideshow/anmeldelser
+
+let slideNummer = 1;
+
+visSlides(slideNummer);
+
+function plusSlides(n) {
+    visSlides(slideNummer += n);
+}
+
+function visSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("kunde");
+    if (n > slides.length) {
+        slideNummer = 1
+    }
+    if (n < 1) {
+        slideNummer = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideNummer - 1].style.display = "block";
+}
--- javascript/script.js
+++ javascript/script.js
@@ -3,44 +3,11 @@ window.addEventListener("load", sidenVises);
 function sidenVises() {
     console.log("sidenVises");
 
-    // Gem pop op
-    document.querySelector("#popop").classList.add("hide");
-
-    // Vis pop op når man trykker på pil
-    document.querySelector("#pil").addEventListener("click", visPopop);
-
-    // Vis pop op efter 15 sekunder (hvis nu brugeren ikke bruger "pil"-knappen)
-    setTimeout(visPopop, 30000);
-
     // Tryk på burgermenu
     document.querySelector("#menuburger").addEventListener("click", toggelmenu);
 
     // Tryk på "behandlinger og priser"
     document.querySelector("#behandlinger_knap").addEventListener("click", toggelDropdown);
-}
-
-function visPopop() {
-    console.log("visPopop");
-
-    // Ryd op
-    document.querySelector("#pil").removeEventListener("click", visPopop);
-
-    // Vis pop op
-    document.querySelector("#popop").classList.remove("hide");
-
-    // Gør det muligt at lukke pop op
-    document.querySelector("#luk").addEventListener("click", lukPopop);
-
-}
-
-function lukPopop() {
-    console.log("lukPopop");
-
-    // Ryd op:
-    document.querySelector("#luk").removeEventListener("click", lukPopop);
-
-    // Skjul pop op
-    document.querySelector("#popop").classList.add("hide");
 }
 
 function toggelmenu() {
@@ -61,6 +28,4 @@ function toggelDropdown() {
     console.log("toggelDropdown");
     document.querySelector("#dropdown").classList.toggle("hide");
     document.querySelector("#dropdown").classList.toggle("hidden");
-
-
 }
